f08967f9f2c5a364609b56ddacdbf37c38bd3a36
Leonardo Rodrigues <donderileo@gmail.com>
2021-10-10 16:03:54 -0300

    .bat para setar path do gcc e rodar teste semantico/gerador, ambos apontando para o projeto gerador

---
+++ semantico.bat
@@ -0,0 +1,2 @@
+set PATH=C:\MinGW\bin;%PATH%
+java -jar compiladores-corretor-automatico-1.0-SNAPSHOT-jar-with-dependencies.jar "java -jar gerador\target\gerador-1.0-SNAPSHOT-jar-with-dependencies.jar" gcc ./ casos-de-teste "754756" semantico
